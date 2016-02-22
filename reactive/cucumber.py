041fe6b6a55f7ee52df0e98db45bd9185b4ea8a5
Charles Butler <chuck@dasroot.net>
2016-02-22 12:13:45 -0500

    Commit of wrap up work illustrating relations

--- reactive/cucumber.py
+++ reactive/cucumber.py
@@ -3,36 +3,28 @@ from charms.reactive import when, when_not, set_state
 # interrogate config, and discern if a config option has
 # changed
 from charmhelpers.core.hookenv import config, status_set
-from charmhelpers.fetch import install_remote
+from charmhelpers.fetch.archiveurl import ArchiveUrlFetchHandler as auf
 
 # reactive works with synethetic states
 @when_not('cucumber.installed')
 def install_cucumber():
     # User messaging, that the charm is not configured
     if not config('rebacca_url'):
-       status_set('blocked', 'rebacca_url not configured')
-       return
+        status_set('blocked', 'rebacca_url not configured')
+        return
     if not config('rebacca_sum'):
-       status_set('blocked', 'rebacca_sum not configured')
-       return
-  
+        status_set('blocked', 'rebacca_sum not configured')
+        return
+
     # curl's the file passed as first argument, and verifies with
-    # second argument 
-    install_remote(config('rebacca_url'), checksum=config('rebacca_sum'))
- 
+    # second argument
+    fetcher = auf()
+    fetcher.install(config('rebacca_url'), checksum=config('rebacca_sum'),
+                    hash_type='sha256')
     set_state('cucumber.installed')
 
 
-@when_not('java.installed')
-def install_java():
-    # install java
-    pass
-
-@when('java.installed')
-def proceed_with_consuming_java():
-    pass
-
 @when('cucumber.installed')
 def probe_nfv():
-   # kick off teh nfv evaluation
-   pass 
+    # kick off the nfv evaluation
+    pass
